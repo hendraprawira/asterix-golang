4935bbafb3bec523a08cf0d4976af4058be111a6
Hendra <prawirah@aqi.co.id>
2023-07-31 09:08:04 +0700

    feat(router) : add gin framework

---
+++ app/router/router.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"asterix-golang/app/controllers/asterix"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Routes() *gin.Engine {
+	// gin.SetMode(gin.ReleaseMode) // for release mode, uncomment if need it
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST", "OPTIONS", "GET", "PUT", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+
+	// Define a route handler for handling HTTP requests
+	apiUri := r.Group("/v1")
+	asterixRoute := apiUri.Group("")
+	{
+		asterixRoute.GET("/geosocket", asterix.WebSocket)
+	}
+
+	return r
+}
--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"asterix-golang/app/router"
 	"asterix-golang/app/utils"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 	"golang.org/x/net/ipv4"
 )
@@ -18,49 +17,20 @@ func main() {
 		fmt.Println(err)
 	}
 
-	port := os.Getenv("UDP_PORT")
+	portUdp := os.Getenv("UDP_PORT")
+	port := ":" + os.Getenv("ACTIVE_PORT")
 	recHost := os.Getenv("HOST_UDP_REC")
-	conn := utils.ConnectionUDP(port)
+
+	conn := utils.ConnectionUDP(portUdp)
 	packetConn := ipv4.NewPacketConn(conn)
 	buffer := make([]byte, 10048)
 	defer conn.Close()
 
 	go utils.ReadUDP(packetConn, buffer, recHost)
 
-	http.HandleFunc("/geosocket", WebSocket)
-	fmt.Println("WebSocket server is running on http://localhost:8000/ws")
-	http.ListenAndServe(":8080", nil)
+	if err := router.Routes().Run(port); err != nil {
+		log.Fatalln(err)
+	}
+
 	select {}
-}
-
-func WebSocket(w http.ResponseWriter, r *http.Request) {
-	conn, err := utils.Upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("[BE-Tracking-Surface-Debug] [err] [controllers-tracking] [WebSocketGetDataSurface] [Upgrade Connection]: ", err)
-		return
-	}
-
-	defer conn.Close()
-	utils.GlobalWebSocketCon = conn // Store the connection globally
-
-	for {
-		// Read message from WebSocket
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("[BE-Tracking-Surface-Debug] [err] [controllers-tracking] [WebSocketGetDataSurface] [Read Message]: ", err)
-			if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
-				log.Println("WebSocket connection closed.")
-			} else {
-				log.Printf("Error reading WebSocket message: %v", err)
-			}
-			break
-		}
-		log.Println("Received message from WebSocket:", string(message))
-		response := []byte("Received your message: " + string(message))
-		err = conn.WriteMessage(websocket.TextMessage, response)
-		if err != nil {
-			log.Println("[BE-Tracking-Surface-Debug] [err] [controllers-tracking] [WebSocketGetDataSurface] [Send Responese]: ", err)
-			break
-		}
-	}
 }
