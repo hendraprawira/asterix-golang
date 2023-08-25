4f07f73458ae74f7f1ac219c4d272d32e470203a
Muhammad Hutomo Padmanaba <mhutomop@gmail.com>
2023-08-25 12:59:25 +0700

    perf(websocket): fix multiclients & fix handle DC

--- app/router/router.go
+++ app/router/router.go
@@ -1,25 +1,63 @@
 package router
 
 import (
-	"asterix-golang/app/utils"
 	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true }, // Allow any origin
+}
+
+// Define a structure to represent connected clients.
+type Client struct {
+	conn *websocket.Conn
+}
+
+var clients = make(map[*Client]bool)
+
 func SetupRoutes(router *gin.Engine, wsChan chan []byte) {
 	// Define the WebSocket route
 	router.GET("/geosocket", func(c *gin.Context) {
-		upgrader := websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool { return true }, // Allow any origin
-		}
 		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 		if err != nil {
 			log.Println("Error upgrading to WebSocket:", err)
 			return
 		}
-		utils.HandleWebSocket(ws, wsChan)
+
+		defer ws.Close()
+
+		client := &Client{conn: ws}
+		clients[client] = true
+
+		for {
+			_, _, err := ws.ReadMessage()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+		}
+
+		delete(clients, client)
 	})
+
+	go HandleWebSocket(clients, wsChan)
+}
+
+func HandleWebSocket(clients map[*Client]bool, wsChan <-chan []byte) {
+	for data := range wsChan {
+		for client := range clients {
+			err := client.conn.WriteMessage(websocket.TextMessage, data)
+			if err != nil {
+				log.Println("Error sending data over WebSocket:", err)
+				client.conn.Close()
+				delete(clients, client)
+				break
+			}
+		}
+	}
 }
--- app/utils/udp.go
+++ app/utils/udp.go
@@ -2,10 +2,7 @@ package utils
 
 import (
 	"fmt"
-	"log"
 	"net"
-
-	"github.com/gorilla/websocket"
 
 	"golang.org/x/net/ipv4"
 )
@@ -40,21 +37,6 @@ func ProcessData(dataChan <-chan []byte, wsChan chan<- []byte) {
 		if int(data[0:1][0]) == 240 && len(data) > 400 {
 			datas := AsterixGeoJSONParse(data)
 			wsChan <- datas
-		}
-	}
-}
-
-func HandleWebSocket(ws *websocket.Conn, wsChan <-chan []byte) {
-	go SendMessages(ws, wsChan)
-}
-
-func SendMessages(ws *websocket.Conn, wsChan <-chan []byte) {
-	defer ws.Close()
-	for data := range wsChan {
-		err := ws.WriteMessage(websocket.TextMessage, data)
-		if err != nil {
-			log.Println("Error sending data over WebSocket:", err)
-			break
 		}
 	}
 }
